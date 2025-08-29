ba7531815884739cf4d36036414eb162fb5ffcb7
Yael Franco Toledo <143737848+YaelF05@users.noreply.github.com>
2025-08-29 13:52:35 -0600

    Add files via upload

---
+++ ejemplos_ethan/src/Main.java
@@ -0,0 +1,22 @@
+import clasesAbstractas.*;
+import interfaces.Pato;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pato miPatito = new Pato(2);
+        miPatito.hacerRuido();
+        miPatito.volar();
+
+//        Camion miCamion = new Camion("Ford", "4", "rojo", 4);
+//        miCamion.acelerar();
+//        miCamion.encender();
+
+//        Carro miCarrito = new Carro ("a","a","q");
+
+//        Perro miPerro = new Perro("perrito", 10);
+//        miPerro.comer();
+//        miPerro.ladrar();
+
+    }
+}
