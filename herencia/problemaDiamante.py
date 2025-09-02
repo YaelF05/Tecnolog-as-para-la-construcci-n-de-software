b8f8eba9b5a026fdd1554b70aafef448e5c88196
Yael Franco Toledo <143737848+YaelF05@users.noreply.github.com>
2025-09-02 13:44:02 -0600

    Add files via upload

---
+++ herencia/problemaDiamante.py
@@ -0,0 +1,20 @@
+class A:
+    def method (self):
+        print("From A")
+
+class B(A):
+    def method (self):
+        print("From B")
+
+class C(A):
+    def method (self):
+        print("From C")
+
+class D (B,C):
+    def hello (self):
+        print("From D")
+
+d = D()
+
+print(D.mro)
+d.method()
